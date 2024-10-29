8c7e57c475f790a08069156961b56a59cb3ad6ad
Alina <a.kotlovets@lntu.edu.ua>
2024-10-29 19:28:17 +0200

    create section skill desctop

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,5 +1,5 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 4,
+    slidesPerView: 3,
     spaceBetween: 32,
         breakpoints: {
             1200: {
@@ -15,6 +15,25 @@ var swiper = new Swiper(".mySwiper", {
                 spaceBetween: 16
             }
         },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".mySwiperSkill", {
+    slidesPerView: 3,
+    spaceBetween: 32,
+    breakpoints: {
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 32
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        }
+    },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
