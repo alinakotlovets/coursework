03280c05a9313553a215acf91d46c3a39e3198b6
Alina <a.kotlovets@lntu.edu.ua>
2024-10-29 16:44:51 +0200

    choose course mob/tab adapt and popular course desct + slider

--- docs/js/slider.js
+++ docs/js/slider.js
@@ -1,1 +1,1 @@
-const swiper=new Swiper(".swiper-container",{slidesPerView:4,spaceBetween:30,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",clickable:!0},loop:!0});
+var swiper=new Swiper(".mySwiper",{slidesPerView:4,spaceBetween:32,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,13 +1,8 @@
-const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 4, // Кількість видимих слайдів одночасно
-    spaceBetween: 30, // Проміжок між слайдами (в пікселях)
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 32,
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    loop: true, // Безкінечний цикл
 });
