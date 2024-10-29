9f31d55d3f0a84c0e9219c5be7381701bd0cc794
Alina <a.kotlovets@lntu.edu.ua>
2024-10-29 18:25:17 +0200

    choose course mob/tab adapt

--- docs/js/slider.js
+++ docs/js/slider.js
@@ -1,1 +1,1 @@
-var swiper=new Swiper(".mySwiper",{slidesPerView:4,spaceBetween:32,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+var swiper=new Swiper(".mySwiper",{slidesPerView:4,spaceBetween:32,breakpoints:{1200:{slidesPerView:4,spaceBetween:32},768:{slidesPerView:3,spaceBetween:20},420:{slidesPerView:4,spaceBetween:16}},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
