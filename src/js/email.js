0d5e0121455d4af6c99d790e3ade4dc4f702f3b3
Alina <a.kotlovets@lntu.edu.ua>
2024-11-08 19:12:09 +0200

    some chsnges for courswork

--- src/js/email.js
+++ src/js/email.js
@@ -1,21 +1,19 @@
 function sendEmail() {
     const email = document.getElementById('email').value;
     const errorMessage = document.getElementById('error-message');
-
-    // Регулярний вираз для перевірки формату email
     const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
-    // Перевіряємо, чи підходить email під шаблон
     if (!emailRegex.test(email)) {
         errorMessage.style.display = 'block';
-        errorMessage.innerText = 'Введіть правильну пошту';
-        return; // Зупиняємо виконання, якщо формат неправильний
+        errorMessage.innerText = 'Write right email';
+        setTimeout(function() {
+            errorMessage.style.display = 'none';
+        }, 1000);
+        return;
     }
 
-    // Якщо формат правильний, ховаємо повідомлення про помилку
     errorMessage.style.display = 'none';
 
-    // Відправляємо email через EmailJS
     let params = {
         email: email
     };
@@ -23,17 +21,12 @@ function sendEmail() {
     emailjs.send("service_1tuftii","template_m4c31eg", params)
         .then(function (response) {
             document.getElementById('contact-form').style.display = 'none';
+            document.getElementById('subscribe-title').style.display = 'none';
             document.getElementById('overlay').style.display = 'block';
+            setTimeout(function() {
+                document.getElementById('overlay').style.display = 'none';
+            }, 1000);
         })
 }
 
-// Функція для показу повідомлення
-// function showMessage(id) {
-//     var message = document.getElementById(id);
-//     message.style.display = 'block';
-//
-//     // Приховуємо повідомлення через 10 секунд
-//     setTimeout(() => {
-//         message.style.display = 'none';
-//     }, 1000); // 10000 мілісекунд = 10 секунд
-// }
+
